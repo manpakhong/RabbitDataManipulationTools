f57795ddfb26ef8ad1db0d521515527dde8e4d54
davephman <davephman@oaitd1374.hkbh.org.hk>
2019-10-14 17:43:18 +0800

    bug fixed: null pointer exception

--- src/main/com/rabbitforever/datamanipulation/views/RestoreMainView.java
+++ src/main/com/rabbitforever/datamanipulation/views/RestoreMainView.java
@@ -68,171 +68,182 @@ public class RestoreMainView extends MainScreenView {
 
 	@Override
 	protected void init() {
-		captureScopeVo = new CaptureScopeVo();
+		try {
+			captureScopeVo = new CaptureScopeVo();
 
-		mainFrame = new JFrame("Restore Main Screen");
-		mainPanel = new JPanel();
-		mainPanel.setLayout(new FlowLayout());
+			mainFrame = new JFrame("Restore Main Screen");
+			mainPanel = new JPanel();
+			mainPanel.setLayout(new FlowLayout());
 
-		// JLabel label = new JLabel("This is a label!");
-		// mainPanel.add(label);
+			// JLabel label = new JLabel("This is a label!");
+			// mainPanel.add(label);
 
-		scopeNameLabel = new JLabel();
-		scopeNameLabel.setText("scope file name:");
-		mainPanel.add(scopeNameLabel);
+			scopeNameLabel = new JLabel();
+			scopeNameLabel.setText("scope file name:");
+			mainPanel.add(scopeNameLabel);
 
-		scopeNameTextField = new JTextField();
-		scopeNameTextField.setPreferredSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
-		mainPanel.add(scopeNameTextField);
+			scopeNameTextField = new JTextField();
+			scopeNameTextField.setPreferredSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+			mainPanel.add(scopeNameTextField);
 
-		scopeFolderLabel = new JLabel();
-		scopeFolderLabel.setText("scope folder:");
-		mainPanel.add(scopeFolderLabel);
+			scopeFolderLabel = new JLabel();
+			scopeFolderLabel.setText("scope folder:");
+			mainPanel.add(scopeFolderLabel);
 
-		scopeFolderTextField = new JTextField();
-		scopeFolderTextField.setPreferredSize(new Dimension(TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT));
-		mainPanel.add(scopeFolderTextField);
+			scopeFolderTextField = new JTextField();
+			scopeFolderTextField.setPreferredSize(new Dimension(TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT));
+			mainPanel.add(scopeFolderTextField);
 
-		// addButton = new JButton();
-		// addButton.setText("Add");
-		// mainPanel.add(addButton);
-		//
-		// editButton = new JButton();
-		// editButton.setText("Edit");
-		// mainPanel.add(editButton);
-		//
-		//
-		// deleteButton = new JButton();
-		// deleteButton.setText("Delete");
-		// mainPanel.add(deleteButton);
+			// addButton = new JButton();
+			// addButton.setText("Add");
+			// mainPanel.add(addButton);
+			//
+			// editButton = new JButton();
+			// editButton.setText("Edit");
+			// mainPanel.add(editButton);
+			//
+			//
+			// deleteButton = new JButton();
+			// deleteButton.setText("Delete");
+			// mainPanel.add(deleteButton);
 
-		browseFileListButton = new JButton();
-		browseFileListButton.setText("Browse file list");
-		mainPanel.add(browseFileListButton);
+			browseFileListButton = new JButton();
+			browseFileListButton.setText("Browse file list");
+			mainPanel.add(browseFileListButton);
 
-		viewButton = new JButton();
-		viewButton.setText("View");
-		mainPanel.add(viewButton);
+			viewButton = new JButton();
+			viewButton.setText("View");
+			mainPanel.add(viewButton);
 
-		saveButton = new JButton();
-		saveButton.setText("Save");
-		mainPanel.add(saveButton);
+			saveButton = new JButton();
+			saveButton.setText("Save");
+			mainPanel.add(saveButton);
 
-		restoreButton = new JButton();
-		restoreButton.setText("Restore");
-		mainPanel.add(restoreButton);
+			restoreButton = new JButton();
+			restoreButton.setText("Restore");
+			mainPanel.add(restoreButton);
 
-		if (isDelStmtGenButtonEnabled) {
-			verAndGenDelStatementsButton = new JButton();
-			verAndGenDelStatementsButton.setText("Generate Del. Stmts");
-			mainPanel.add(verAndGenDelStatementsButton);
-		} else {
-			if (deleteThenRestoreComboActionEnabled) {
-				StringBuilder sbMessage = new StringBuilder();
-				sbMessage.append("deleteThenRestoreComboActionEnabled=true, but isDelStmtGenButtonEnabled=false!");
-				sbMessage.append(
-						"\nRestore button will not be enabled until you change sys.properties isDelStmtGenButtonEnabled=true");
-				JOptionPane.showMessageDialog(this.mainFrame, sbMessage.toString());
+			if (isDelStmtGenButtonEnabled) {
+				verAndGenDelStatementsButton = new JButton();
+				verAndGenDelStatementsButton.setText("Generate Del. Stmts");
+				mainPanel.add(verAndGenDelStatementsButton);
+			} else {
+				if (deleteThenRestoreComboActionEnabled) {
+					StringBuilder sbMessage = new StringBuilder();
+					sbMessage.append("deleteThenRestoreComboActionEnabled=true, but isDelStmtGenButtonEnabled=false!");
+					sbMessage.append(
+							"\nRestore button will not be enabled until you change sys.properties isDelStmtGenButtonEnabled=true");
+					JOptionPane.showMessageDialog(this.mainFrame, sbMessage.toString());
+				}
 			}
+
+			if (restoreButton != null) {
+				if (sysProperties == null) {
+					sysProperties = PropertiesFactory.getInstanceOfSysProperties();
+				}
+				boolean deleteThenRestoreEnabled = sysProperties.getDeleteThenRestoreComboActionEnabled();
+				if (deleteThenRestoreEnabled) {
+					restoreButton.setEnabled(false);
+				}
+			}
+
+			closeButton = new JButton();
+			closeButton.setText("Close");
+			mainPanel.add(closeButton);
+
+			mainTable = new CaptureVoJTable(this);
+
+			// String [] columnNames = {
+			// "is Valid delete",
+			// "Table Name",
+			// "xml File Name",
+			// "Capture Sql",
+			// "Actual Table Assertion Sql",
+			// "Actual Table Ass. Ignore Cols"
+			// };
+
+			tableModel = new DefaultTableModel() {
+				@Override
+				public Class<?> getColumnClass(int column) {
+					switch (column) {
+					case 0:
+						return Boolean.class;
+					case 1:
+						return String.class;
+					case 2:
+						return String.class;
+					case 3:
+						return String.class;
+					case 4:
+						return String.class;
+					case 5:
+						return String.class;
+
+					default:
+						return String.class;
+					}
+				}
+			};
+			// tableModel.setColumnIdentifiers(columnNames);
+			tableModel.addColumn("is Valid delete");
+			tableModel.addColumn("Table Name");
+			tableModel.addColumn("xml File Name");
+			tableModel.addColumn("Capture Sql");
+			tableModel.addColumn("Actual Table Assertion Sql");
+			tableModel.addColumn("Actual Table Ass. Ignore Cols");
+
+			mainTable.setModel(tableModel);
+			final TableCellRenderer renderer = mainTable.getDefaultRenderer(Object.class);
+
+			mainTable.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+
+				@Override
+				public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+						boolean hasFocus, int row, int column) {
+
+					
+					
+					Component c = renderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+					Object col1Value =table.getValueAt(row, 0);
+					Boolean isCellEditable = table.isCellEditable(row, 0);
+					Boolean isSelectedValid = null;
+					if (col1Value != null){
+						isSelectedValid = (Boolean) col1Value;
+					}
+					if (!isCellEditable){
+						c.setForeground(Color.BLACK);
+//						c.setEnabled(false);				
+					} else {
+						c.setForeground(Color.RED);
+//						c.setEnabled(false);			
+					}
+
+					return c;
+				}
+
+			});
+			removeAllRowsFromTableModel();
+			bindCaptureVo2TableModel();
+
+			JScrollPane scroll = new JScrollPane(mainTable);
+			scroll.setPreferredSize(new Dimension(MAINTABLE_WIDTH, MAINTABLE_HEIGHT));
+			mainPanel.add(scroll);
+
+			addEventHandlers();
+
+			mainFrame.add(mainPanel);
+			mainFrame.setSize(MAIN_FRAME_WIDTH, MAIN_FRAME_HEIGHT);
+			mainFrame.setLocationRelativeTo(null);
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Color color = new Color(sysProperties.getColorRgbR(), sysProperties.getColorRgbG(), sysProperties.getColorRgbG());
+			UiHelper.setColor(mainPanel,color);
+		} catch (Exception e) {
+			logger.error(className + ".init()", e);
+			JOptionPane.showMessageDialog(this.getMainFrame(),
+					"Some wrong! please refer to log!");
 		}
+		
 
-		if (restoreButton != null) {
-			boolean deleteThenRestoreEnabled = sysProperties.getDeleteThenRestoreComboActionEnabled();
-			if (deleteThenRestoreEnabled) {
-				restoreButton.setEnabled(false);
-			}
-		}
-
-		closeButton = new JButton();
-		closeButton.setText("Close");
-		mainPanel.add(closeButton);
-
-		mainTable = new CaptureVoJTable(this);
-
-		// String [] columnNames = {
-		// "is Valid delete",
-		// "Table Name",
-		// "xml File Name",
-		// "Capture Sql",
-		// "Actual Table Assertion Sql",
-		// "Actual Table Ass. Ignore Cols"
-		// };
-
-		tableModel = new DefaultTableModel() {
-			@Override
-			public Class<?> getColumnClass(int column) {
-				switch (column) {
-				case 0:
-					return Boolean.class;
-				case 1:
-					return String.class;
-				case 2:
-					return String.class;
-				case 3:
-					return String.class;
-				case 4:
-					return String.class;
-				case 5:
-					return String.class;
-
-				default:
-					return String.class;
-				}
-			}
-		};
-		// tableModel.setColumnIdentifiers(columnNames);
-		tableModel.addColumn("is Valid delete");
-		tableModel.addColumn("Table Name");
-		tableModel.addColumn("xml File Name");
-		tableModel.addColumn("Capture Sql");
-		tableModel.addColumn("Actual Table Assertion Sql");
-		tableModel.addColumn("Actual Table Ass. Ignore Cols");
-
-		mainTable.setModel(tableModel);
-		final TableCellRenderer renderer = mainTable.getDefaultRenderer(Object.class);
-
-		mainTable.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
-
-			@Override
-			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-					boolean hasFocus, int row, int column) {
-
-				
-				
-				Component c = renderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-				Object col1Value =table.getValueAt(row, 0);
-				Boolean isCellEditable = table.isCellEditable(row, 0);
-				Boolean isSelectedValid = null;
-				if (col1Value != null){
-					isSelectedValid = (Boolean) col1Value;
-				}
-				if (!isCellEditable){
-					c.setForeground(Color.BLACK);
-//					c.setEnabled(false);				
-				} else {
-					c.setForeground(Color.RED);
-//					c.setEnabled(false);			
-				}
-
-				return c;
-			}
-
-		});
-		removeAllRowsFromTableModel();
-		bindCaptureVo2TableModel();
-
-		JScrollPane scroll = new JScrollPane(mainTable);
-		scroll.setPreferredSize(new Dimension(MAINTABLE_WIDTH, MAINTABLE_HEIGHT));
-		mainPanel.add(scroll);
-
-		addEventHandlers();
-
-		mainFrame.add(mainPanel);
-		mainFrame.setSize(MAIN_FRAME_WIDTH, MAIN_FRAME_HEIGHT);
-		mainFrame.setLocationRelativeTo(null);
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Color color = new Color(sysProperties.getColorRgbR(), sysProperties.getColorRgbG(), sysProperties.getColorRgbG());
-		UiHelper.setColor(mainPanel,color);
 	}
 
 
